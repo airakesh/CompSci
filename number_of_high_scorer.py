56a762fa1342ea64f8867c7438b189dcb9496e02
Rakesh Kumar <40352675+airakesh@users.noreply.github.com>
2019-11-26 21:34:11 +0530

    Number of High Scorer in record

---
+++ number_of_high_scorer.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @params A: a file containing scores received by students in a number of subjects
+    # returns: an integer: a number of students have aggregates scores of 100 or more.
+    # output.txt file contains 2, if two students have aggregates scores of 100 or more.
+    def numberOfHighScorer(self, A):
+        data = {}
+        with open(A, 'r') as file:
+            for line in file:
+                id, subject, marks = line.strip().split(',')
+                print(id, subject, marks)
+                data[id] = data.get(id, 0) + int(marks)
+
+            # Prints student IDs and their total marks who scored => 100.
+            print(len([key for key, value in data.items() if value >= 100]))
+
+# Use cases
+if __name__ == '__main__':
+    s = Solution()
+    A = 'input.txt'
+    s.numberOfHighScorer(A)
