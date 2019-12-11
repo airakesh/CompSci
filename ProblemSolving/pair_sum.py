55a605b16b51a2a7c5421c0bb526d303409a21a5
Rakesh Kumar <40352675+airakesh@users.noreply.github.com>
2019-12-11 20:36:36 +0530

    Find if there are two elements wtih given sum

--- ProblemSolving/pair_sum.py
+++ ProblemSolving/pair_sum.py
@@ -3,22 +3,17 @@
 # Auxiliary Space: O(n) where n is size of array.
 
 class Solution:
-    # @param A : 
-    # @return an integer
-
-    # Method 1: O(n)
-
-    # def pairSum(self, A, k):
-    # 
-    #     s = set()
-    # 
-    #     for i in range(len(A)):
-    #         temp = k - A[i]
-    # 
-    #         if temp in s:
-    #             print((A[i], temp))
-    # 
-    #         s.add(A[i])
+     def pairSum(self, A, k):
+     
+         s = set()
+     
+         for i in range(len(A)):
+             temp = k - A[i]
+     
+             if temp in s:
+                print((A[i], temp))
+     
+             s.add(A[i])
 
 # Test cases
 if __name__ == "__main__":
